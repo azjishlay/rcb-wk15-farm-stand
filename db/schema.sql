9fdcdbcf25f4f928f339f46d6a11dd95fa4104ac
lijasminej <li.jasmine.j@gmail.com>
2016-05-14 04:09:16 -0400

    Reset schema.sql for sequelize

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,2 @@
 CREATE DATABASE IF NOT EXISTS `farmstand`;
 USE `farmstand`;
-
-CREATE TABLE `inventory` (
-	`id` INT(11) AUTO_INCREMENT NOT NULL,
-	`item` VARCHAR(255) NOT NULL,
-	`category` VARCHAR(255) NOT NULL,
-	`price` DECIMAL(8,2),
-	`quantity` INT(11)
-
-	PRIMARY KEY ( `id` ) );
